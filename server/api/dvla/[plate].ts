020f932b5344339ddaa76242d07dfab508187818
rich-tribeweb <rich@tribeweb.co.uk>
2025-12-17 15:01:55 +0000

    fix: hide api key

--- server/api/dvla/[plate].ts
+++ server/api/dvla/[plate].ts
@@ -1,13 +1,16 @@
 export default defineEventHandler(async (event) => {
   const plate = getRouterParam(event, 'plate')
 
+  const config = useRuntimeConfig()
+  const apiKey = config.dvlaApiKeyTest // Use test key for development
+
   const data = JSON.stringify({ registrationNumber: plate })
 
   const response = await $fetch('https://uat.driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles', {
     method: 'POST',
     body: data,
     headers: {
-      'x-api-key': 'AcnarWQtgm6enzwYk4Kis6M9L5vYLL2S6yT778U2',
+      'x-api-key': apiKey,
       'Content-Type': 'application/json'
     }
   })
